812ee0aa11a5a27d15a6bd1fd0c7062705fb0a52
Tibo Vermeire <116001957+TiboVermeire@users.noreply.github.com>
2023-08-21 14:37:40 +0200

    feat: start- stop scan

--- script.js
+++ script.js
@@ -1,48 +1,73 @@
-// the link to your model provided by Teachable Machine export panel
+// script.js
+
 const URL = "https://teachablemachine.withgoogle.com/models/iG8PuTx-H/";
 
 let model, webcam, labelContainer, maxPredictions;
+let isScanning = false;
 
-// Load the image model and setup the webcam
 async function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
+    const flip = true;
+    webcam = new tmImage.Webcam(400, 400, flip);
+    await webcam.setup();
     window.requestAnimationFrame(loop);
 
-    // append elements to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
     labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
+    for (let i = 0; i < maxPredictions; i++) {
         labelContainer.appendChild(document.createElement("div"));
+    }
+
+    toggleScan();
+}
+
+function toggleScan() {
+  const button = document.getElementById("button");
+  if (isScanning) {
+      button.textContent = "Start scan";
+      webcam.stop();
+      clearResults();
+      location.reload(); // Reload the page
+  } else {
+      button.textContent = "Stop scan";
+      webcam.play();
+  }
+  isScanning = !isScanning;
+}
+
+function clearResults() {
+    for (let i = 0; i < maxPredictions; i++) {
+        const labelElement = labelContainer.childNodes[i];
+        labelElement.innerHTML = "";
     }
 }
 
 async function loop() {
-    webcam.update(); // update the webcam frame
-    await predict();
+    webcam.update();
+    if (isScanning) {
+        await predict();
+    }
     window.requestAnimationFrame(loop);
 }
 
-// run the webcam image through the image model
 async function predict() {
-    // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
+        const classPrediction = prediction[i].className;
+        const probability = prediction[i].probability;
+
+        const labelElement = labelContainer.childNodes[i];
+        labelElement.innerHTML = classPrediction + ": " + probability.toFixed(2);
+
+        if (probability > 0.85) {
+            labelElement.style.color = "#00FF00"; // Fluorescent green color
+        } else {
+            labelElement.style.color = "black"; // Reset to default color
+        }
     }
 }
